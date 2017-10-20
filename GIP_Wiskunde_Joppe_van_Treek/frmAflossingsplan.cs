8b201df6fba64df78fe5094ef0e4377205329614
Damoit455 <Gamingstyles159@gmail.com>
2017-10-20 09:14:40 +0200

    Added excel files to form

--- GIP_Wiskunde_Joppe_van_Treek/frmAflossingsplan.cs
+++ GIP_Wiskunde_Joppe_van_Treek/frmAflossingsplan.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace GIP_Wiskunde_Joppe_van_Treek
 {
